63e4ed3dfaa8a78c8c6e9624887ba19a629af032
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-09-05 11:15:03 -0500

    .env file

---
+++ lab-beeksma/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const debug = require('debug')('app:server');
+
+require('dotenv').load();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+const PORT = process.env.PORT;
+if (!PORT){
+  throw new Error('You forgot your .env!');
+}
+if(!module.parent){
+  app.listen(PORT, () => {
+    debug(`listening on ${PORT}`);
+  });
+}
+
+module.exports = app;
