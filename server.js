7f8bb41980721757edb4b45f6f55b84842d5ee86
Meepedy <david@psyon.org>
2017-09-05 11:15:09 -0500

    added .env file

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const morgan = require('morgan');
 const cors = require('cors');
 const debug = require('debug')('app:server');
 
+require('dotenv').load();
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
 
 const PORT = process.env.PORT;
+if(!PORT) {
+  throw new Error("You forgot your .env!");
+}
 
 if (!module.parent){
   app.listen(PORT, () => {
